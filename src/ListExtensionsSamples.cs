9aa3b36d338da2d4c51f39b963b32a50d876a4b5
Franco Melandri <franco.melandri@gmail.com>
2020-07-07 10:11:03 +0200

    adding fold example

---
+++ src/ListExtensionsSamples.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpikeLanguageExt
+{
+    public class ListExtensionsSamples
+    {
+        public T Compute<T>(T[] data, T startFrom, Func<T, T, T> action)
+            => data
+                   .Fold(startFrom, 
+                         (acc, item) => action(acc,item));
+    }
+}
---
+++ test/ListExtensionsTests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SpikeLanguageExt;
+using System;
+
+namespace SpikeLanguageExtTest
+{
+    public class ListExtensionsTests
+    {
+        private ListExtensionsSamples _sut;
+
+        Func<int, int, int> sum = (x, y) => x + y;
+        Func<int, int, int> mul = (x, y) => x * y;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sut = new ListExtensionsSamples();
+        }
+
+        [Test]
+        public void Fold_ShouldSum_Array_Items()
+        {
+            var arr = new[] { 1, 2, 3 };
+            
+            var result = _sut.Compute(arr, 0, sum);
+
+            result.Should().Be(6);
+        }
+
+        [Test]
+        public void Fold_ShouldSum_Array_Items_FromInitValue()
+        {
+            var arr = new[] { 1, 2, 3 };
+            
+            var result = _sut.Compute(arr, 10, sum);
+
+            result.Should().Be(16);
+        }
+
+        [Test]
+        public void Fold_ShouldMul_Array_Items()
+        {
+            var arr = new[] { 1, 2, 3, 4 };
+
+            var result = _sut.Compute(arr, 1, mul);
+
+            result.Should().Be(24);
+        }
+
+        [Test]
+        public void Fold_ShouldMul_Array_Items_FromInitValue()
+        {
+            var arr = new[] { 1, 2, 3, 4 };
+
+            var result = _sut.Compute(arr, 10, mul);
+
+            result.Should().Be(240);
+        }
+    }
+}
